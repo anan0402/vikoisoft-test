f23f2e747f7ecc6f215cb1caa3ff91c37f28ecf6
An <anan04022000@gmail.com>
2023-07-07 18:13:20 +0700

    Successfully just upload TXT file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { DragDrop } from "./components/DragDrop";
 import { ChatBox } from "./components/ChatBox";
+import classNames from "classnames/bind";
+import style from "./AppStyles.module.scss";
+const cx = classNames.bind(style);
 
 function App() {
   return (
-    <div>
+    <div className={cx("wrapper")}>
       <DragDrop />
       <ChatBox />
     </div>
--- src/components/DragDrop/index.tsx
+++ src/components/DragDrop/index.tsx
@@ -1,27 +1,94 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FaFileUpload } from "react-icons/fa";
 import classNames from "classnames/bind";
 import style from "./style.module.scss";
 const cx = classNames.bind(style);
 
-//icon duoi sgv
-
-interface Props {}
 export function DragDrop() {
+  //All states
+  const [active, setActive] = useState(false);
+  const [file, setFile] = useState<File | undefined>();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  //Functions that handle all the upload file events
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setActive(true);
+  };
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setActive(false);
+  };
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setFile(e.dataTransfer.files[0]);
+    console.log(e.dataTransfer.files[0]);
+  };
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setActive(true);
+      setFile(e.target.files[0]);
+      console.log(e.target.files[0]);
+    }
+  };
+  const handleOnClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+  const handelCancel = () => {
+    setFile(undefined);
+    setActive(false);
+  };
+
+  if (file && file.type === "text/plain") {
+    return (
+      <>
+        <button className={cx("cancel-btn")} onClick={handelCancel}>
+          Delete
+        </button>
+      </>
+    );
+  }
+
   return (
     <div className={cx("container")}>
-      <h1 className={cx("heading")}>Basic Drag and Drop</h1>
+      <h1 className={cx("heading")}>Basic Drag and Drop </h1>
       <div className={cx("drag-content")}>
         <h3 className={cx("title")}>Upload your file</h3>
-        <div className={cx("drag-area")}>
+        <div
+          className={cx("drag-area")}
+          onDragOver={(e: React.DragEvent<HTMLDivElement>) => handleDragOver(e)}
+          onDragLeave={(e: React.DragEvent<HTMLDivElement>) =>
+            handleDragLeave(e)
+          }
+          onDrop={(e: React.DragEvent<HTMLDivElement>) => handleDrop(e)}
+          style={{
+            border: active ? "1px solid #1e78f7 " : "1px dashed #e0eafc",
+          }}
+        >
           <div className={cx("icon")}>
             <FaFileUpload />
           </div>
-          <span className={cx("text")}>Drag & Drop</span>
           <span className={cx("text")}>
-            or<span className={cx("button")}>browse</span>
+            Drag and drop document here to upload
           </span>
-          <span className={cx("support")}>Support for 'file.txt'</span>
+          <input
+            type="file"
+            multiple
+            hidden
+            onChange={handleOnChange}
+            ref={inputRef}
+          />
+          <button className={cx("select-btn")} onClick={handleOnClick}>
+            Select from device
+          </button>
+          <span className={cx("support")}>
+            Upload document support for TXT files
+          </span>
         </div>
       </div>
     </div>
